f923ad7d3de6f78ebe195fdc615780f38eb5fa6e
phithanhha <phithanhha15072002@gmail.com>
2023-08-22 09:20:37 +0700

    EnterAddItem

--- src/App.js
+++ src/App.js
@@ -3,36 +3,52 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todo, setTodo] = useState([
+  const [todoList, setTodoList] = useState([
     { id: 1, title: "hello", status: false },
     { id: 2, title: "hi", status: false },
   ]);
 
-  const [addItem, setAddItem] = useState("");
+  const [addInputItem, setAddInputItem] = useState("");
+
+  document.addEventListener(function (event) {
+    if (event.keyCode === 13) {
+      console.log("hi ha document");
+      console.log(event);
+    }
+  });
+
+  window.addEventListener("DOMContentLoaded", (event) => {
+    // =.addEventListener("keydown", function (event) {
+    //   if (event.keyCode === 13) {
+    //     console.log("hi ha");
+    //     console.log(event);
+    //   }
+    // });
+  });
 
   const addDatasItem = () => {
-    if (addItem) {
-      let index = todo.length + 1;
-      let newItem = { id: index, title: addItem, status: false };
-      setTodo([...todo, newItem]);
-      setAddItem("");
+    if (addInputItem) {
+      let index = todoList.length + 1;
+      let newItem = { id: index, title: addInputItem, status: false };
+      setTodoList([...todoList, newItem]);
+      setAddInputItem("");
     }
   };
 
   const deleteItem = (id) => {
-    let deletedItem = todo.filter((item) => item.id !== id);
-    setTodo(deletedItem);
+    let deletedItem = todoList.filter((item) => item.id !== id);
+    setTodoList(deletedItem);
     console.log("deletedItem", deletedItem);
   };
 
   const completed = (id) => {
-    const completedItem = todo.map((item) => {
+    const completedItem = todoList.map((item) => {
       if (item.id === id) {
         return { ...item, status: !item.status };
       }
       return item;
     });
-    setTodo(completedItem);
+    setTodoList(completedItem);
   };
 
   return (
@@ -42,19 +58,20 @@ function App() {
           <h1 className="heading">My Todos</h1>
           <div className="box-add">
             <input
-              value={addItem}
-              onChange={(e) => setAddItem(e.target.value)}
+              value={addInputItem}
+              onChange={(e) => setAddInputItem(e.target.value)}
               className="input-add"
               placeholder="Add something to do..."
+              id="input-add-id"
             />
             <button className="btn-add" onClick={addDatasItem}>
               Save
             </button>
           </div>
 
-          {todo && todo.length ? "" : "No tasks..."}
+          {todoList && todoList.length ? "" : "No tasks..."}
 
-          {todo.map((items) => {
+          {todoList.map((items) => {
             return (
               <div key={items.id}>
                 <div className="list-item">
